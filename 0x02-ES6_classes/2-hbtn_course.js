e4e45a20d273921811256ed8d803a6cd4a99aab1
amisoni70 <amisoni7014@gmail.com>
2024-03-07 13:01:33 +0300

    Task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if ((typeof name !== 'string') && !(name instanceof String)) {
+      throw new TypeError('Name must be a string');
+    }
+    if ((typeof length !== 'number') && !(length instanceof Number)) {
+      throw new TypeError('Length must be a number.');
+    }
+    if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    return this._newName;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(newLength) {
+    return this._newLength;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(newStudents) {
+    return this._newStudents;
+  }
+}
