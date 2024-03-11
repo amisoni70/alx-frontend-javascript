d72fe4d863e7aa6bff3d01efce4bebc20d9d0f0b
amisoni70 <amisoni7014@gmail.com>
2024-03-11 18:06:54 +0300

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, startString) {
+  if (startString === '' || typeof startString !== 'string') return '';
   return [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.substring(startString.length))
